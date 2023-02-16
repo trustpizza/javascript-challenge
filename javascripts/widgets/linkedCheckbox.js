f37d914d5a621f2893c8218a6ad7fad8e3527de0
trustpizza <maxelolsson@gmail.com>
2023-02-16 09:43:15 -0600

    INstall all node packages

--- javascripts/widgets/linkedCheckbox.js
+++ javascripts/widgets/linkedCheckbox.js
@@ -1,3 +1,9 @@
 function linkedCheckbox(widget) {
+    // Select all checkbox items
 
+    // setup function
+        // Setup function needs to set the current checkbox item 
+        // Set a variable to equal the current checkbox item's children (and their children, etc. etc. until all elements are selected)
+        // Set a variable equal to a widget's state (checked, unchecked, intermediary)
+        
 }
