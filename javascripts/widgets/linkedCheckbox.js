b1a10eebd26be278de33a25108594f40e6cdbb36
trustpizza <maxelolsson@gmail.com>
2023-02-16 09:48:04 -0600

    Pseudocode the linkedCheckbox > setup function

--- javascripts/widgets/linkedCheckbox.js
+++ javascripts/widgets/linkedCheckbox.js
@@ -5,5 +5,20 @@ function linkedCheckbox(widget) {
         // Setup function needs to set the current checkbox item 
         // Set a variable to equal the current checkbox item's children (and their children, etc. etc. until all elements are selected)
         // Set a variable equal to a widget's state (checked, unchecked, intermediary)
+
+    // What does the setup function need to do? It needs
+    /* 
+        The setup function needs to create the logic for a checkbox. 
+        The logic for a checkbox is:
+            1. Each box must have a list of child elements (this list can be null)
+            2. Each box must have a parent element (this can also be null)
+            3. Each box must have a state (checked, unchecked, intermediary)
+
+        There must also be a function that handles state changes
+        checkboxClickHandler () {
+            This function needs to do the following:
+            1. Search the node list of child elements 
+        }
+    */
         
 }
